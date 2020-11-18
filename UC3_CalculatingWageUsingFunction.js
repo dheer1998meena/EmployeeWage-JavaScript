47dd0b7fda99b6aafb1c6bac15ab8e21f17c2161
dheer meena <dheer1998meena@gmail.com>
2020-11-18 11:14:48 +0530

    [Dheer] Add: Implemented the functionality to refactor the code to calculating wage using functions

---
+++ UC3_CalculatingWageUsingFunction.js
@@ -0,0 +1,69 @@
+console.log("Welcome to Employee Wage Problem \n")
+//UC1 Ability to check Employees is present or absent.
+console.log("UC1 Checking employee is present or absent ");
+const IS_ABSENT = 0;
+let empCheck = Math.floor(Math.random()*10)%3;
+{
+    if(empCheck == IS_ABSENT)
+    {
+        console.log("Employee is absent");
+    }
+    else 
+    {
+        console.log("Employee is present");
+    }
+}
+
+//UC2 Ability to Calculate Daily Employee Wage based on part time or full time work.
+{
+console.log("UC2 Calculating wage for employee");
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS =4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+let empHours = 0;
+let empCheck = Math.floor(Math.random()*10)%3;
+switch(empCheck)
+{
+    case IS_PART_TIME:
+    console.log("Employee is working part time");
+    empHours=PART_TIME_HOURS;
+    break;
+    case IS_FULL_TIME:
+    console.log("Employee is working full time");
+    empHours = FULL_TIME_HOURS;
+    break;
+    default:
+    empHours =0;
+    break;
+}
+let empWage = empHours*WAGE_PER_HOUR;
+console.log("Employee wage: " + empWage);
+}
+//UC3 Refactor the Code to write a function to get work hours.
+  console.log("UC3 Refactor the Code to write a function to get work hours");
+  const IS_PART_TIME = 1;
+  const IS_FULL_TIME = 2;
+  const PART_TIME_HOURS =4;
+  const FULL_TIME_HOURS = 8;
+  const WAGE_PER_HOUR = 20;
+  function getWorkingHrs(empCheck)
+  {
+      switch (empCheck) 
+      {
+          case IS_PART_TIME:
+              console.log("Employee is working part time");
+              return PART_TIME_HOURS;          
+          case IS_FULL_TIME:
+              console.log("employee is working full time");
+              return FULL_TIME_HOURS;
+          default:       
+              return 0;
+      }
+  }
+  
+  let empWage = WAGE_PER_HOUR*getWorkingHrs(empCheck);
+  console.log("Employee wage: " + empWage+"\n");
+    
+
